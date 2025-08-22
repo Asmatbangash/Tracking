d53ab4a3481ea5c16f91bfe2345369b0e1c53a6b
Asmat Ullah <bangashasmat877@gmail.com>
2025-08-22 16:27:40 +0500

    intall vite pickage for fast loading images.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgo from "vite-plugin-svgo";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgo({
+      plugins: [
+        { removeViewBox: false }, // keep viewBox for responsiveness
+        { cleanupIDs: true },
+        { removeDimensions: true }, // allows width/height via CSS
+      ],
+    }),
+  ],
 });
